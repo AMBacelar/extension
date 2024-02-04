304bed41056ba86b754ca8ca95468a1690614464
Adilson Bacelar <a.m.bacelar@gmail.com>
2024-02-04 23:05:25 +0000

    further progress on the popup

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,40 +1,48 @@
 import type { Manifest } from 'webextension-polyfill';
 import pkg from '../package.json';
 
-const manifest: Manifest.WebExtensionManifest = {
-  manifest_version: 3,
-  name: pkg.displayName,
-  version: pkg.version,
-  description: pkg.description,
-  // options_ui: {
-  //   page: "src/pages/options/index.html",
-  // },
-  background: {
-    service_worker: 'src/pages/background/index.js',
-    type: 'module',
-  },
-  action: {
-    default_popup: 'src/pages/popup/index.html',
-    default_icon: 'icon.png',
-  },
-  icons: {
-    '128': 'icon.png',
-  },
-  permissions: ['storage', 'identity', 'identity.email'],
-  content_scripts: [
-    {
-      matches: ['<all_urls>'],
-      js: ['src/pages/content/index.js'],
-      css: ['contentStyle.css'],
+const manifest: Manifest.WebExtensionManifest & { oauth2: any; key?: string } =
+  {
+    manifest_version: 3,
+    name: pkg.displayName,
+    version: pkg.version,
+    description: pkg.description,
+    oauth2: {
+      client_id:
+        '479734964735-t5gmdfnqcfv2uj204rq1qiinpvh7mirg.apps.googleusercontent.com',
+      scopes: [
+        'openid',
+        'email',
+        'profile',
+        'https://www.googleapis.com/auth/gmail.readonly',
+      ],
     },
-  ],
-  // devtools_page: 'src/pages/devtools/index.html',
-  web_accessible_resources: [
-    {
-      resources: ['contentStyle.css', 'icon.png'],
-      matches: [],
+    background: {
+      service_worker: 'src/pages/background/index.js',
+      type: 'module',
     },
-  ],
-};
+    action: {
+      default_popup: 'src/pages/popup/index.html',
+      default_icon: 'icon.png',
+    },
+    icons: {
+      '128': 'icon.png',
+    },
+    permissions: ['storage', 'identity', 'identity.email'],
+    content_scripts: [
+      {
+        matches: ['<all_urls>'],
+        js: ['src/pages/content/index.js'],
+        css: ['contentStyle.css'],
+      },
+    ],
+    // devtools_page: 'src/pages/devtools/index.html',
+    web_accessible_resources: [
+      {
+        resources: ['contentStyle.css', 'icon.png'],
+        matches: [],
+      },
+    ],
+  };
 
 export default manifest;
--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,29 +1,171 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import logo from "@assets/img/logo.svg";
+import loader from "@assets/img/loading.gif";
+import mango from "@assets/img/mango.png";
+import zara from "@assets/img/zara.png";
+import asos from "@assets/img/asos.png";
+import houseOfCb from "@assets/img/house_of_cb.png";
 
+const screens = ['signIn', 'firstTimeUser', 'loadingEmails', 'startShopping', 'returningUser'] as const;
+type Screen = typeof screens[number];
 export default function Popup(): JSX.Element {
-  return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
-      <header className="flex flex-col items-center justify-center text-white">
+  const [screen, setScreen] = useState<Screen>('signIn');
+  const [isFirstTimeUser, setIsFirstTimeUser] = useState<boolean>(true);
+  console.log('yolo')
+  const handleLogin = async () => {
+    // check whether there are emails
+    // if there are no emails, show the first time user screen
+    setScreen('firstTimeUser')
+  }
+
+  const handleLogout = () => {
+    setScreen('signIn')
+  }
+
+  const handleLoadEmails = () => {
+    setScreen('loadingEmails')
+  }
+
+  const SignIn = () => (
+    <>
+      <header className="flex flex-col items-center justify-center text-black">
         <img
           src={logo}
-          className="h-36 pointer-events-none animate-spin-slow"
+          className="h-[56px] pointer-events-none"
           alt="logo"
         />
-        <p>
-          Edit <code>src/pages/popup/Popup.jsx</code> and save to reload.
-        </p>
-        <p>testing</p>
-        <a
-          className="text-blue-400"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <p>Popup styled with TailwindCSS!</p>
+        <h1>Hello!</h1>
+        <p>Let&#39;s get started</p>
+        <button onClick={handleLogin}>Google</button>
+        <p>Now Available at</p>
+        <div className="flex items-center gap-8">
+          <img className="w-[85px] h-[14px]" src={mango} />
+          <img className="w-[49px] h-[19px]" src={zara} />
+          <img className="w-[97px] h-[27px]" src={houseOfCb} />
+        </div>
+        <a href="">Browse our brand directory</a>
       </header>
+    </>)
+
+  const FirstTimeUser = () => (
+    <>
+      <header className="flex flex-col items-center justify-center text-black">
+        <img
+          src={logo}
+          className="h-[56px] pointer-events-none"
+          alt="logo"
+        />
+        <h1>Hello, NAME</h1>
+        <p>efitter uses your past orders to predict your size.<br />
+          To get started, click “load your emails”</p>
+        <button onClick={handleLoadEmails}>Load your email</button>
+        <a href="https://www.efitterapp.com/how-it-works"
+          target="_blank" rel="noreferrer">How it works</a>
+        <button onClick={handleLogout}>Log out</button>
+      </header>
+    </>
+  )
+
+  const LoadingEmails = () => {
+    useEffect(() => {
+      setTimeout(() => {
+        if (isFirstTimeUser) {
+          setScreen('startShopping')
+        } else {
+          setScreen('returningUser')
+        }
+      }, 1000);
+    }, [])
+
+    return (
+      <>
+        <header className="flex flex-col items-center justify-center text-black">
+          <img
+            src={logo}
+            className="h-[56px] pointer-events-none"
+            alt="logo"
+          />
+          <h1>Hello, NAME</h1>
+          <p>Give us a sec, searching for items...</p>
+          <img
+            src={loader}
+            className="h-[114px] pointer-events-none"
+            alt="logo"
+          />
+          <p>email count</p>
+          <a href="https://www.efitterapp.com/how-it-works"
+            target="_blank" rel="noreferrer">How it works</a>
+          <button onClick={handleLogout}>Log out</button>
+        </header>
+      </>
+    )
+  }
+
+  const StartShopping = () => (
+    <>
+      <header className="flex flex-col items-center justify-center text-black">
+        <img
+          src={logo}
+          className="h-[56px] pointer-events-none"
+          alt="logo"
+        />
+        <h1>Hello, NAME</h1>
+        <p>Now you&#39;re ready to shop!<br />
+          Start shopping at your favorite brands including</p>
+        <div className="flex items-center gap-8">
+          <a href="https://www.houseofcb.com/" target="_blank" rel="noreferrer"><img className="w-[121px] h-[35px]" src={houseOfCb} /></a>
+          <a href="https://www.zara.com/uk/" target="_blank" rel="noreferrer"><img className="w-[86px] h-[32px]" src={zara} /></a>
+          <a
+            href="https://www.asos.com/women/a-to-z-of-brands/asos-collection/cat/?cid=4877&nlid=ww|brands|top+brands|asos+brands"
+            target="_blank" rel="noreferrer"
+          ><img className="9-[35px] h-[38px]" src={asos} /></a>
+        </div>
+        <a href="https://www.efitterapp.com/brand-directory"
+          target="_blank" rel="noreferrer">Browse our brand directory</a>
+        <button onClick={handleLoadEmails}>Load more emails</button>
+        <a href="https://www.efitterapp.com/how-it-works"
+          target="_blank" rel="noreferrer">How it works</a>
+        <button onClick={handleLogout}>Log out</button>
+      </header>
+    </>
+  )
+
+  const ReturningUser = () => (
+    <>
+      <header className="flex flex-col items-center justify-center text-black">
+        <img
+          src={logo}
+          className="h-[56px] pointer-events-none"
+          alt="logo"
+        />
+        <h1>Hello, NAME</h1>
+        <p>You&#39;re ready to shop!<br />
+          Click any of the brands below to start shopping</p>
+        <div className="flex items-center gap-8">
+          <a href="https://www.houseofcb.com/" target="_blank" rel="noreferrer"><img className="w-[121px] h-[35px]" src={houseOfCb} /></a>
+          <a href="https://www.zara.com/uk/" target="_blank" rel="noreferrer"><img className="w-[86px] h-[32px]" src={zara} /></a>
+          <a
+            href="https://www.asos.com/women/a-to-z-of-brands/asos-collection/cat/?cid=4877&nlid=ww|brands|top+brands|asos+brands"
+            target="_blank" rel="noreferrer"
+          ><img className="9-[35px] h-[38px]" src={asos} /></a>
+        </div>
+        <a href="https://www.efitterapp.com/brand-directory"
+          target="_blank" rel="noreferrer">Browse our brand directory</a>
+        <button onClick={handleLoadEmails}>Load more emails</button>
+        <a href="https://www.efitterapp.com/how-it-works"
+          target="_blank" rel="noreferrer">How it works</a>
+        <button onClick={handleLogout}>Log out</button>
+      </header>
+    </>
+  )
+
+  return (
+    <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-[600px] w-[572px] p-3 bg-white">
+      {screen === 'signIn' && <SignIn />}
+      {screen === 'firstTimeUser' && <FirstTimeUser />}
+      {screen === 'loadingEmails' && <LoadingEmails />}
+      {screen === 'startShopping' && <StartShopping />}
+      {screen === 'returningUser' && <ReturningUser />}
     </div>
   );
 }
